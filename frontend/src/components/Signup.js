60e4bd948863c5fc942f038b9e47d159860ca015
vjspranav <pranavasri@live.in>
2021-05-17 14:45:06 +0530

    class -> className`

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -4,42 +4,51 @@ import { useState } from "react";
 function Signup() {
   return (
     <div>
-      <div class="user">
-        <header class="user__header">
+      <div className="user">
+        <header className="user__header">
           <img
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3219/logo.svg"
             alt=""
           />
-          <h1 class="user__title">Welcome! 😄</h1>
+          <h1 className="user__title">Welcome! 😄</h1>
         </header>
 
-        <form class="form">
-          <div class="form__group">
+        <form className="form">
+          <div className="form__group">
             <input
               id="username"
               type="text"
               placeholder="Username"
-              class="form__input"
+              className="form__input"
             />
           </div>
 
-          <div class="form__group">
-            <input id="" type="email" placeholder="Email" class="form__input" />
+          <div className="form__group">
+            <input
+              id=""
+              type="email"
+              placeholder="Email"
+              className="form__input"
+            />
           </div>
 
-          <div class="form__group">
-            <input type="password" placeholder="Password" class="form__input" />
+          <div className="form__group">
+            <input
+              type="password"
+              placeholder="Password"
+              className="form__input"
+            />
           </div>
 
-          <div class="form__group">
+          <div className="form__group">
             <input
               type="password"
               placeholder="Confirm Password"
-              class="form__input"
+              className="form__input"
             />
           </div>
 
-          <button class="btn" type="button">
+          <button className="btn" type="button">
             Register
           </button>
         </form>
